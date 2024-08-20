6ecf9936e88be99e6bff0f08a99a4a157be1593f
Suhani Kelapure <118129572+SuhaniKelapure08@users.noreply.github.com>
2024-08-20 17:45:35 +0530

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
         <div className="flex flex-col xl:flex-row gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
-            href="https://amazon-clone-d8jrfiit4-suhanikelapure08s-projects.vercel.app/"
+            href="https://amazon-clone-fawn-seven.vercel.app/"
             target="_blank"
           >
             <div>
